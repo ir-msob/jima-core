5188f064975a3c6c1d89802a87e312e02d0de309
yaghob abdi <y.abdi@rashasoft.ir>
2024-04-01 01:00:58 +0330

    1.1.10

--- web/angular/ng-publish.sh
+++ web/angular/ng-publish.sh
@@ -2,6 +2,10 @@ npm install
 for project in $(ls projects/); do
   cd projects/$project
   npm install
+  ng build
+  cd ../..
+  cd dist/$project
+  npm link
   cd ../..
   ng test --project=$project --watch=false --browsers=ChromeHeadless
   cd projects/$project
