29b05033e43404201f9ee528f2b5a5e83615e957
yaghob abdi <y.abdi@rashasoft.ir>
2025-12-19 23:54:14 +0330

    SQL

--- ral/hr/it/src/main/java/ir/msob/jima/core/ral/hr/it/test/TestDomain.java
+++ ral/hr/it/src/main/java/ir/msob/jima/core/ral/hr/it/test/TestDomain.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.*;
+
 @Setter
 @Getter
 @ToString(callSuper = true)
@@ -35,6 +37,26 @@ public class TestDomain implements ProjectDomain {
     @Column(name = "domain_field", nullable = false)
     private String domainField;
 
+    // --- Embeddable example ---
+    @Embedded
+    private AddressInfo address;
+
+    // --- ManyToOne (owner) ---
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    // --- OneToMany children (bidirectional) ---
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<Child> children = new ArrayList<>();
+
+    // --- ManyToMany tags via join table ---
+    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.LAZY)
+    @JoinTable(name = "test_domain_tag",
+            joinColumns = @JoinColumn(name = "domain_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private Set<Tag> tags = new HashSet<>();
+
     public TestDomain(String id) {
         this.id = id;
     }
@@ -42,4 +64,84 @@ public class TestDomain implements ProjectDomain {
     public enum FN {
         domainField
     }
+
+    @Setter
+    @Getter
+    @ToString(callSuper = true)
+    @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Embeddable
+    public static class AddressInfo {
+        @Column(name = "addr_street")
+        private String street;
+
+        @Column(name = "addr_city")
+        private String city;
+
+        @Column(name = "addr_zip")
+        private String zip;
+    }
+
+    @Setter
+    @Getter
+    @ToString(callSuper = true)
+    @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Entity
+    @Table(name = "owner")
+    public static class Owner {
+        @Id
+        @GeneratedValue(strategy = GenerationType.UUID)
+        private String id;
+        private String name;
+    }
+
+    @Setter
+    @Getter
+    @ToString(callSuper = true)
+    @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Entity
+    @Table(name = "child")
+    public static class Child {
+        @Id
+        @GeneratedValue(strategy = GenerationType.UUID)
+        private String id;
+
+        private String name;
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "parent_id")
+        private TestDomain parent;
+    }
+
+    @Setter
+    @Getter
+    @ToString(callSuper = true)
+    @NoArgsConstructor
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @Entity
+    @Table(name = "tag")
+    public static class Tag {
+        @Id
+        @GeneratedValue(strategy = GenerationType.UUID)
+        private String id;
+
+        @Column(unique = true)
+        private String name;
+
+        // equals/hashCode based on name if used in Set
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Tag tag)) return false;
+            return Objects.equals(name, tag.name);
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(name);
+        }
+    }
+
 }
+
